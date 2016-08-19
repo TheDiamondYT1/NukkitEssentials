8bd38ff0d4c205a33112a7b7f1246f74c5521213
TheDiamondYT1 <goldowl37@gmail.com>
2016-08-19 14:56:43 +0100

    Forgot to include file

---
+++ src/main/java/net/thediamondyt/essentials/commands/CommandBroadcast.java
@@ -0,0 +1,42 @@
+/*
+ *
+ *   __________         ______                                   ____      _________
+ *   |__   __| |        |  __ \(_)                               | \ \   / /__   __|
+ *      | |  | |__   ___| |  | |_  __ _ _ __ ___   ___  _ __   __| |\ \_/ /   | |
+ *      | |  | '_ \ / _ \ |  | | |/ _` | '_ ` _ \ / _ \| '_ \ / _` | \   /    | |
+ *      | |  | | | |  __/ |__| | | (_| | | | | | | (_) | | | | (_| |  | |     | |
+ *      |_|  |_| |_|\___|_____/|_|\__,_|_| |_| |_|\___/|_| |_|\__,_|  |_|     |_|
+ *
+ *
+ * NukkitEssentials v1.0
+ */
+package net.thediamondyt.essentials.commands;
+
+import cn.nukkit.command.CommandSender;
+
+import net.thediamondyt.essentials.Main;
+
+public class CommandBroadcast extends EssentialsCommand {
+
+    public CommandBroadcast(Main plugin) {
+        super(plugin, "broadcast", "Broadcast a message to the server", "<message>");
+        setPermission("essentials.broadcast");
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if(!testPermission(sender)) return true;
+
+        if(args.length >= 2) {
+            sender.sendMessage(f("<red>Usage: /broadcast <message>"));
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(String arg : args) {
+            sb.append(arg + " ");
+        }
+        sender.getServer().broadcastMessage(f("<red>Broadcast > <white>" + sb.toString()));
+        return true;
+    }
+}
